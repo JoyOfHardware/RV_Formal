2f18babfcf1bb0d32b749d214cd0e6b6e7f0e9d4
Yehowshua Immanuel <yehowshua@yehowshuaimmanuel.com>
2024-05-21 20:06:20 -0400

    now loading firmware in correct order

--- hs/Peripherals/Ram.hs
+++ hs/Peripherals/Ram.hs
@@ -10,11 +10,12 @@ import Clash.Prelude
 import qualified Prelude as P
 import qualified Data.ByteString.Lazy as BL
 import Data.Binary.Get ( getInt32le, runGet, isEmpty )
-import Data.Int (Int32)
+import Data.Int (Int32, Int8)
 import qualified Clash.Sized.Vector as Vec
 import Bus(Request(..), Resp(..))
 import Types(Addr, Byte, HalfWord, FullWord, DoubleWord, QuadWord)
 import Util(fullWordsToQuadWords)
+import Data.Binary (Word8)
 
 #ifndef _RAM_DEPTH
 #define _RAM_DEPTH 1024
@@ -32,45 +33,38 @@ ramWidth = 128
 initRamFromFile :: FilePath -> IO (Maybe Ram)
 initRamFromFile filePath = 
     let
-      zeroedRamVec32 = Vec.replicate (SNat :: SNat (_RAM_DEPTH * 4)) 0
+      zeroedRamVec8 = Vec.replicate (SNat :: SNat (_RAM_DEPTH * 16)) (0 :: Unsigned 8)
     in
       do
         bs <- readFileIntoByteString filePath
-        let int32s = zeroPadMkLengthMultipleOfFour $ byteStringToInt32s bs
-        let initRamVec32 = populateVectorFromInt32s int32s zeroedRamVec32
-        pure $ case initRamVec32 of
-          Just vec32 -> Just (fullWordsToQuadWords vec32)
+        let int8List = zeroPadMkLengthMultipleOf16 $ byteStringToInt8List bs
+        let initRamVec8 = populateVectorFromInt8s int8List zeroedRamVec8
+        pure $ case initRamVec8 of
+          Just vec8 -> Just $ bitCoerce vec8
           Nothing -> Nothing
 
 readFileIntoByteString :: FilePath -> IO BL.ByteString
 readFileIntoByteString filePath = BL.readFile filePath
 
-byteStringToInt32s :: BL.ByteString -> [Int32]
-byteStringToInt32s bs = runGet listOfInts bs
-  where
-    listOfInts = do
-      empty <- isEmpty
-      if empty
-        then pure []
-      else do
-        i <- getInt32le    -- Parse a single Int32 from the stream
-        rest <- listOfInts -- Recursively parse the rest
-        pure (i : rest)
+byteStringToInt8List :: BL.ByteString -> [Int8]
+byteStringToInt8List bs = P.map (fromIntegral :: Word8 -> Int8) (BL.unpack bs)
 
-populateVectorFromInt32s :: 
+populateVectorFromInt8s :: 
     KnownNat n =>
-    [Int32] -> 
-    Vec n (Unsigned 32) -> 
-    Maybe (Vec n (Unsigned 32))
-populateVectorFromInt32s ls v = Vec.fromList adjustedLs
+    [Int8] -> 
+    Vec n (Unsigned 8) -> 
+    Maybe (Vec n (Unsigned 8))
+populateVectorFromInt8s ls v = Vec.fromList adjustedLs
   where
     vecLen = length v
     adjustedLs = fromIntegral <$> adjustLength vecLen ls
-    adjustLength :: Int -> [Int32] -> [Int32]
+    adjustLength :: Int -> [Int8] -> [Int8]
     adjustLength n xs = P.take n (xs P.++ P.repeat 0)
 
-zeroPadMkLengthMultipleOfFour :: [Int32] -> [Int32]
-zeroPadMkLengthMultipleOfFour xs = xs P.++ (P.replicate padding 0)
+zeroPadMkLengthMultipleOf16 :: [Int8] -> [Int8]
+zeroPadMkLengthMultipleOf16 xs = xs P.++ (P.replicate padding 0)
   where
     len = P.length xs
-    padding = (4 - (len `mod` 4)) `mod` 4
+    padding = if multipleOf16 then 0 else 16 - remainder
+    remainder = len `mod` 16
+    multipleOf16 = remainder == 0
